258cbc0496743195e94a14905eeac22395d9b431
user <user@user-PC>
2015-02-01 18:33:46 +0200

    First commit

---
+++ src/watermelon/WaterMelon.java
@@ -0,0 +1,16 @@
+package watermelon;
+
+import java.io.*;
+
+public class WaterMelon {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int a;
+        
+        a = Integer.parseInt(br.readLine());
+        if ((a%2 == 0) && (a > 2)){System.out.print("YES");}
+        else {System.out.print("NO");}
+    }
+}
